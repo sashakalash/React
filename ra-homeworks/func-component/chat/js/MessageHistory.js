60485569005df59016ff9aa60d174d4f79c1e10a
Саша Калаш <kalash@Sasha-Kalash.local>
2018-09-15 17:26:04 +0300

    messageHistory func

--- ra-homeworks/func-component/chat/js/MessageHistory.js
+++ ra-homeworks/func-component/chat/js/MessageHistory.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const MessageHistory = ({list}) => {
+  if (list.length === 0) {
+    return null;
+  }
+
+  const ChatItem = list.map(item => {
+    const ChatMessage = item.type !== 'message' ?
+      item.type === 'response' ? Response : Typing : 
+        Message;
+    return <ChatMessage key={item.id} from={item.from} message={item} />;
+  });
+  
+  return <ul>{ChatItem}</ul>;
+}
+
+MessageHistory.defaultProps = {
+  list: []
+}
