807a8325a57a224756681a9c822a6a66c72db42a
Саша Калаш <kalash@Sasha-Kalash.local>
2018-09-15 14:05:18 +0300

    films rating component edited

--- ra-homeworks/func-component/films/js/Stars.js
+++ ra-homeworks/func-component/films/js/Stars.js
@@ -1,5 +1,10 @@
 'use strict';
 
-function Stars() {
+function Stars({count}) {
+  if ((count < 1 || count > 5) || typeof count !== 'number') {
+    return null;
+  }
   return <ul className="card-body-stars u-clearfix"><li><Star /></li></ul>;
 }
+
+Stars.defaultProps = {count: 0}
