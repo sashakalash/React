9bb152200436a9f3a0f0f8e5057c8bcc57132e48
Саша Калаш <kalash@Sasha-Kalash.local>
2018-09-16 14:42:24 +0300

    HexInput component corrected

--- ra-homeworks/events/hex2rgb/js/HexInput.js
+++ ra-homeworks/events/hex2rgb/js/HexInput.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const HexInput = props => {
-  function onChange() {
-    props.onChange(this.value);
+  function onChange(event) {
+    props.onChange(event.currentTarget.value);
   }
 
   return (
