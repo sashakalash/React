30c3a4f6c31063b95e4c3482b162958e79014f87
Саша Калаш <kalash@Sasha-Kalash.local>
2018-09-17 21:52:15 +0300

    AuthForm component added

--- ra-homeworks/events/auth/js/AuthForm.js
+++ ra-homeworks/events/auth/js/AuthForm.js
@@ -1,1 +1,68 @@
 'use strict';
+
+const emailRegEx = /[^\w@._-]/;
+const passRegEx = /[^\w_]/;
+
+const checkInput = e => {
+  const regEx = e.currentTarget.type === 'email' ? emailRegEx : passRegEx;
+  e.currentTarget.value = e.currentTarget.value.replace(regEx, '');
+}
+
+const AuthForm = ({onAuth}) => {
+  let nameField, emailField, PassField;
+
+  const NameBlock = props => (
+    <div className="Input">
+      <input ref={element => nameField = element} required type="text" placeholder="Имя" />
+      <label></label>
+    </div>
+  )
+
+  const EmailBlock = props => (
+    <div className="Input">
+      <input 
+      ref={element => emailField = element} 
+      onChange={checkInput}
+      type="email" 
+      placeholder="Электронная почта" 
+      />
+      <label></label>
+    </div>
+  )
+
+  const PassBlock = props => (
+    <div className="Input">
+      <input 
+      ref={element => PassField = element} 
+      onChange={checkInput}
+      required 
+      type="password" 
+      placeholder="Пароль" 
+      />
+      <label></label>
+    </div>
+  )
+
+  const SubmitForm = e => {
+    e.preventDefault();
+    const userInfo = {
+      name: nameField,
+      email: emailField,
+      password: PassField
+    }
+    const formData = new FormData(userInfo);
+    onAuth && typeof onAuth === 'function'? onAuth(formData): null;
+  }
+
+  return (
+    <form onSubmit={SubmitForm} className="ModalForm" action="/404/auth/" method="POST">
+      <NameBlock />
+      <EmailBlock />
+      <PassBlock />
+      <button onClick={SubmitForm} type="submit">
+        <span>Войти</span>
+        <i className="fa fa-fw fa-chevron-right"></i>
+      </button>
+    </form>
+  )
+}
