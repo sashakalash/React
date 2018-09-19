1d7be781cb0bb3f83dbed785613069c67fa1137f
Саша Калаш <kalash@Sasha-Kalash.local>
2018-09-19 23:13:28 +0300

    filter state component added

--- ra-homeworks/state/filter/js/App.js
+++ ra-homeworks/state/filter/js/App.js
@@ -1,11 +1,31 @@
 'use strict'
 
-const App = props => (
-  <div>
-    <Toolbar
-      filters={props.filters}
-      selected={'All'}
-      onSelectFilter={(filter) => console.log(filter)} />
-    <Portfolio projects={props.projects} />
-  </div>
-);
+class App extends React.Component {
+  constructor (props) {
+		super(props);
+		this.state = {
+      selected: 'All',
+      projects: props.projects
+    };
+  }
+  selectFilter(filter) {
+    this.setState({
+      selected: filter,
+      projects: filter === 'All'
+        ? this.props.projects
+        : this.props.projects.filter(project => project.category === filter)
+    })    
+  }
+  render() {
+    return (
+      <div>
+        <Toolbar
+          filters={this.props.filters}
+          selected={this.state.selected}
+          onSelectFilter={filter => this.selectFilter(filter)} />
+        <Portfolio projects={this.state.projects} />
+      </div>
+    )
+  }
+}
+
