69649c547436b1e5bc1e2358c6ab3726939ef53c
Саша Калаш <kalash@Sasha-Kalash.local>
2018-09-20 23:00:17 +0300

    app component rewrited to ES2015

--- ra-homeworks/state/dropdown/js/App.js
+++ ra-homeworks/state/dropdown/js/App.js
@@ -1,28 +1,29 @@
-var App = React.createClass({
-  defaultProps: {
-    options: []
-  },
-  getInitialState: function () {
-    return {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
       active: this.props.options[0],
       open: false
-    };
-  },
-  handleChange: function (option) {
+    }
+  }
+
+  handleChange(option) {
     this.setState({
       active: option
     });
-  },
-  toggleOpen: function () {
+  }
+
+  toggleOpen() {
     this.setState({
       open: !this.state.open
     });
-  },
-  render: function () {
+  }
+
+  render() {
     return (
       <div className="container">
         <div className={`dropdown-wrapper ${this.state.open ? "open" : ""}`} >
-          <button className={"btn"} onClick={this.toggleOpen} >
+          <button className={"btn"} onClick={() => this.toggleOpen()} >
             <span>Account Settings</span>
             <i className="material-icons">public</i>
           </button>
@@ -31,7 +32,7 @@ var App = React.createClass({
               <li
                 className={option === this.state.active ? "active" : ""}
                 onClick={() => this.handleChange(option)} >
-                  <a href="#">{option}</a>
+                <a href="#">{option}</a>
               </li>
             ))}
           </ul>
@@ -39,4 +40,5 @@ var App = React.createClass({
       </div>
     );
   }
-});
+}
+  
