ace8ee51843af97d1e0f71f2612b044291875e51
Саша Калаш <kalash@Sasha-Kalash.local>
2018-09-25 23:30:45 +0300

    email validation func added

--- ra-homeworks/ctrl-component/subscribeForm/js/SubscribeForm.js
+++ ra-homeworks/ctrl-component/subscribeForm/js/SubscribeForm.js
@@ -1,5 +1,37 @@
-const SubscribeForm = () => {
+
+const ErrorMessage = props => props.isErr
+    ? <div>Пожалуйста, проверьте корректность адреса электронной почты</div>
+    : null;
+
+class SubscribeForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      isValid: '',
+      isErr: false
+    }
+  }
+  render() {
     return (
-        <div>Поиск</div>
+      <form>
+        <input
+          type="email"
+          value={this.state.email}
+          onChange={this.emailValidate.bind(this)}
+          className={this.state.isValid}
+        />
+        <ErrorMessage isErr={this.state.isErr}/>
+      </form>
     )
-};
+  }
+
+  emailValidate(event) {
+    const valid = event.currentTarget.validity.valid;
+    this.setState({
+      email: event.currentTarget.value,
+      isValid: valid ? 'is-valid' : 'is-error',
+      isErr: !valid
+    })
+  }
+}
