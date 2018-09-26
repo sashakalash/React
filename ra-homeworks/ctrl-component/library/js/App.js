4dbae09c5a9da4cce76f3a55e58cf313ed9de574
Саша Калаш <kalash@Sasha-Kalash.local>
2018-09-26 21:57:36 +0300

    searchBox component edited

--- ra-homeworks/ctrl-component/library/js/App.js
+++ ra-homeworks/ctrl-component/library/js/App.js
@@ -1,43 +1,45 @@
 'use strict';
 
 class App extends React.Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-            query: "React",
-            books: []
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: "React",
+      books: [],
+      author: ''
     }
+  }
 
-    componentDidMount () {
-        BookApi.query(this.state.query).then(books => {
-            this.setState({books})
-        })
-    }
+  componentDidMount() {
+    BookApi.query(this.state.query).then(books => {
+      this.setState({ books })
+    })
+  }
 
-    filterBooks = (query) => {
-        this.setState({query, books:[]});
-        BookApi.query(query).then(books => {
-            this.setState({books})
-        });
-    }
+  filterBooks = (query) => {
+    this.setState({ query, books: [], author: query });
+    BookApi.query(query).then(books => {
+      this.setState({ books })
+    });
+  }
 
-    render () {
-        return (
-            <div className="App">
-                <div className="search-books">
-                    <div className="search-books-bar">
-                        <div className="search-books-input-wrapper">
-                            <SearchBox value={this.state.query}
-                                filterBooks={this.filterBooks}/>
-                        </div>
-                    </div>
-
-                    <div className="search-books-results">
-                        <Bookshelf books={this.state.books} filterBooks={this.filterBooks}/>
-                    </div>
-                </div>
+  render() {
+    return (
+      <div className="App">
+        <div className="search-books">
+          <div className="search-books-bar">
+            <div className="search-books-input-wrapper">
+              <SearchBox value={this.state.query}
+                filterBooks={this.filterBooks} 
+                currentAuthor={this.state.author}/>
             </div>
-        );
-    }
+          </div>
+
+          <div className="search-books-results">
+            <Bookshelf books={this.state.books} filterBooks={this.filterBooks} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
--- ra-homeworks/ctrl-component/library/js/SearchBox.js
+++ ra-homeworks/ctrl-component/library/js/SearchBox.js
@@ -1,5 +1,27 @@
-const SearchBox = () => {
+'use strict';
+
+class SearchBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentSearchText: this.props.value,
+      author: this.props.currentAuthor
+    };
+  }
+  render() {
     return (
-        <input type="text" placeholder="Поиск по названию или автору"/>
+      <input
+        value={this.props.currentAuthor ? this.props.currentAuthor : this.state.currentSearchText}
+        onChange={this.findBook.bind(this)}
+      />
     );
-};
+  }
+
+  findBook(event) {
+    const name = event.currentTarget.value;
+    this.props.filterBooks(name);
+    this.setState({
+      currentSearchText: name
+    });
+  }
+}
