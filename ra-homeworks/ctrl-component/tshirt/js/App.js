c778d7f467f5e8c4caca3f01704da829047bc047
Саша Калаш <kalash@Sasha-Kalash.local>
2018-09-27 23:37:19 +0300

    +

--- ra-homeworks/ctrl-component/tshirt/js/App.js
+++ ra-homeworks/ctrl-component/tshirt/js/App.js
@@ -2,7 +2,7 @@
 
 class App extends React.Component {
 
-    constructor (props) {
+    constructor(props) {
         super(props);
         this.state = {
             fonts: [],
@@ -11,36 +11,37 @@ class App extends React.Component {
         }
     }
 
-    componentDidMount () {
-        this.setState({fonts: AbcApi.getFonts()});
+    componentDidMount() {
+        this.setState({ fonts: AbcApi.getFonts() });
     }
 
     selectFont = (selectedFont) => {
-        this.setState({selectedFont});
+        this.setState({ selectedFont });
     }
 
-    renderLines () {
+    renderLines() {
         if (!this.state.selectedFont) return null;
         return this.state.text.split("\n").map((line, i) => {
+            console.log(line)
             return (
                 <div className="grid" key={`line-${i}`}>
-                    <PictureFont text={line} path={this.state.selectedFont.path}/>
+                    <PictureFont text={line} path={this.state.selectedFont.path} />
                 </div>
             )
         });
     };
 
-    render () {
+    render() {
         return (
             <div className="App">
                 <div className="select-font">
                     <h2 className={this.state.selectedFont ? 'hidden' : ''}>Выберите шрифт</h2>
                     <FontSelector fonts={this.state.fonts} onSelect={this.selectFont}
-                                  selected={this.state.selectedFont}/>
+                        selected={this.state.selectedFont} text={this.state.text} />
                 </div>
                 {this.state.selectedFont
-                && <TextRenderLine value={this.state.text} onChange={this.setText}/>
-                    }
+                    && <TextRenderLine value={this.state.text} onChange={this.setText} />
+                }
                 {this.state.text && (
                     <div className="render-result">
                         {this.renderLines()}
@@ -51,6 +52,6 @@ class App extends React.Component {
     }
 
     setText = (text) => {
-        this.setState({text});
+        this.setState({ text });
     }
 }
