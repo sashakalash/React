cdf54981487ba375570419f88711e4e482bff043
Саша Калаш <kalash@Sasha-Kalash.local>
2018-09-27 23:38:56 +0300

    added text render logic

--- ra-homeworks/ctrl-component/tshirt/js/TextRenderLine.js
+++ ra-homeworks/ctrl-component/tshirt/js/TextRenderLine.js
@@ -1,7 +1,32 @@
-const TextRenderLine = ({value, onChange}) => {
-	return (
-		<div className="type-text">
-			Наберите текст
-		</div>
-	);
-};
+
+class TextRenderLine extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			text: this.props.value
+		}
+	}
+
+	typeText(event) {
+		this.setState({
+			text: event.currentTarget.value
+		})
+		this.props.onChange(event.currentTarget.value)
+	}
+
+	render() {
+		return (
+			<div className="type-text">
+				<textarea
+					value={this.state.text}
+					onChange={this.typeText.bind(this)}
+					name="text"
+					id="font-text"
+					cols="30"
+					rows="2"
+					placeholder="Введите текст для футболки">
+				</textarea>
+			</div>
+		)
+	}
+}
