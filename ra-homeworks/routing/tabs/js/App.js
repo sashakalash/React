d8c30e9e74473fd0fd9ab3f2553abafd3fdaace3
Саша Калаш <kalash@Sasha-Kalash.local>
2018-09-29 22:19:57 +0300

    route App created

--- ra-homeworks/routing/tabs/js/App.js
+++ ra-homeworks/routing/tabs/js/App.js
@@ -1,1 +1,17 @@
 
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div className="tabs">
+          <Menu/>
+          <div className="tabs__content">
+            <Route path="/essay" component={Essay} />
+            <Route path="/creator" component={Creator} />
+            <Route path="/fortune" component={Fortune} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
