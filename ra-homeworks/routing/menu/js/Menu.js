7d22fbcccb0b6cde507d7b8dc080881e41399413
Саша Калаш <kalash@Sasha-Kalash.local>
2018-09-29 21:59:58 +0300

    Menu nav added

---
+++ ra-homeworks/routing/menu/js/Menu.js
@@ -0,0 +1,12 @@
+class Menu extends React.Component {
+  render() {
+    return (
+      <nav className="menu">
+        <NavLink exact className="menu__item" to="/" activeClassName="menu__item-active">Главная</NavLink>
+        <NavLink exact className="menu__item" to="/drift" activeClassName="menu__item-active">Дрифт-такси</NavLink>
+        <NavLink exact className="menu__item" to="/timeattack" activeClassName="menu__item-active">Time Attack</NavLink>
+        <NavLink exact className="menu__item" to="/forza" activeClassName="menu__item-active">Forza Karting</NavLink>
+      </nav>
+    )
+  }
+}
