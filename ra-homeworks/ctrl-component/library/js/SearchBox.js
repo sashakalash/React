3ea7cf07853537df98b96d4ebfaa11a9edd26e10
Саша Калаш <kalash@Sasha-Kalash.local>
2018-09-30 21:00:00 +0300

    search logic corrected

--- ra-homeworks/ctrl-component/library/js/SearchBox.js
+++ ra-homeworks/ctrl-component/library/js/SearchBox.js
@@ -1,27 +1,18 @@
 'use strict';
 
 class SearchBox extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentSearchText: this.props.value,
-      author: this.props.currentAuthor
-    };
+ 
+  findBook = event => {
+    const name = event.currentTarget.value;
+    this.props.filterBooks(name);
   }
+
   render() {
     return (
       <input
-        value={this.props.currentAuthor ? this.props.currentAuthor : this.state.currentSearchText}
-        onChange={this.findBook.bind(this)}
+        value={this.props.currentAuthor ? this.props.currentAuthor : this.props.value}
+        onChange={this.findBook}
       />
     );
-  }
-
-  findBook(event) {
-    const name = event.currentTarget.value;
-    this.props.filterBooks(name);
-    this.setState({
-      currentSearchText: name
-    });
   }
 }
