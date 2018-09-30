d5afbfefd560fc687f409370a9d7481ff38443cd
Саша Калаш <kalash@Sasha-Kalash.local>
2018-09-30 21:54:09 +0300

    FontSelector logic edited

--- ra-homeworks/ctrl-component/tshirt/js/FontSelector.js
+++ ra-homeworks/ctrl-component/tshirt/js/FontSelector.js
@@ -1,23 +1,21 @@
 'use strict';
 
 class FontSelector extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: 'abc',
-    }
-  }
-
   render() {
-    const fontsList = this.props.fonts.map(item => 
-      <FontItem 
-        onSelect={() => this.props.onSelect(item)}
-        item={item} 
-        text={this.state.text}
-      />
-    )
     return (
-      <div className="font-picker">{fontsList}</div>
+      <div className="font-picker">{
+        this.props.fonts.map((font, index) => {
+          return (
+            <div key={index} className="grid center font-item">
+              <input type="radio" name="font" value={font.name} onChange={() => this.props.onSelect(font)} id={font.name} />
+              <label htmlFor={font.name} className="grid-1">
+                <PictureFont text={"abc"} path={font.path} />
+              </label>
+            </div>
+          );
+        })
+      }
+      </div>
     );
   }
 }
