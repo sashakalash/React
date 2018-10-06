0a3be2825fab4e50b454f336c995e58152c32169
Саша Калаш <kalash@Sasha-Kalash.local>
2018-10-06 22:44:58 +0300

    +

--- ra-homeworks/lifecycle/search/js/SearchBoxView.js
+++ ra-homeworks/lifecycle/search/js/SearchBoxView.js
@@ -1,9 +1,9 @@
-const SearchBoxView = (...props) => (
-  <section ref={props[0].SectionField} className="container">
+const SearchBoxView = (props) => (
+  <section ref={props.SectionField} className="container">
     <div className="row">
       <div className="col-sm-12">
         <input
-          className={`search-box ${props[0].fixed ? 'search-box_fixed' : null}`}
+          className={`search-box ${props.fixed ? 'search-box_fixed' : null}`}
           placeholder="Поиск"
         >
         </input>
