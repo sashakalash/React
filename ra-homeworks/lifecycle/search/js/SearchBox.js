c495b6e3dbdb1a1f79aa0e80c4e242d366bea5f9
Саша Калаш <kalash@Sasha-Kalash.local>
2018-10-06 16:32:10 +0300

    +

--- ra-homeworks/lifecycle/search/js/SearchBox.js
+++ ra-homeworks/lifecycle/search/js/SearchBox.js
@@ -1,12 +1,17 @@
 class SearchBox extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = { fixed: false };
   }
+  checkingBlockDIstance() {
+  }
 
+  componentDidMount() {
+    console.log(this.SearchBox)
+    window.addEventListener('scroll', this.checkingBlockDIstance.bind(this))
+  }
   render() {
-    return <SearchBoxView fixed={this.state.fixed} />
+    return <SearchBoxView ref={el => this.SearchBox = el} fixed={this.state.fixed} />
   }
 
   isFixed() {
