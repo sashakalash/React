144b3d663cddd2043fe5c34a57819f8bbac210f8
Саша Калаш <kalash@Sasha-Kalash.local>
2018-10-07 20:14:53 +0300

    added wrappers for New & Popular Items

--- ra-homeworks/hoc/highlighted-blocks/js/List.js
+++ ra-homeworks/hoc/highlighted-blocks/js/List.js
@@ -1,17 +1,29 @@
 'use strict';
 
-const List = props => {
-    return props.list.map(item => {
-        switch (item.type) {
-            case 'video':
-                return (
-                    <Video {...item} />
-                );
+const GetWrapper = (ComponentsWrapper, Component) => {
+  return class extends React.Component {
+    render() {
+      return ComponentsWrapper
+      ? <ComponentsWrapper><Component {...this.props}/></ComponentsWrapper> 
+      : <Component {...this.props}/>;
+    }
+  }
+}
 
-            case 'article':
-                return (
-                    <Article {...item} />
-                );
-        }
-    });
-};
+const ComponentInWrapper = Component => {
+  return class extends React.Component {
+    render() {
+      const typeOfWrapper = this.props.views < 100 ? New : this.props.views >= 1000 ? Popular : null;
+      const ComponentsWrapper = GetWrapper(typeOfWrapper, Component);
+      return <ComponentsWrapper {...this.props}/>;
+    }
+  }
+}
+
+const setTypeItem = item => {
+  const componentType = item.type == 'video' ? Video : Article;
+  const NewComponent = ComponentInWrapper(componentType);
+  return <NewComponent {...item} /> ;
+}
+
+const List = props => props.list.map(setTypeItem);
