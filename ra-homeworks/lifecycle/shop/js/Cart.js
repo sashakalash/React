91729406161b213e2b10abd72b7f3c3a8963e976
Саша Калаш <kalash@Sasha-Kalash.local>
2018-10-07 13:20:43 +0300

    corrected carts adding items logic

--- ra-homeworks/lifecycle/shop/js/Cart.js
+++ ra-homeworks/lifecycle/shop/js/Cart.js
@@ -1,9 +1,14 @@
 class Cart extends React.Component {
+  
+  shouldComponentUpdate(newProps) {
+    const hasChanges = () => this.props.isOpen !== newProps.isOpen;
+    const itemsCountChanged = () => newProps.isOpen && this.props.items.length !== newProps.items.length;
+    return  hasChanges() || itemsCountChanged();
+  }
 
   render() {
     return (
       <CartView {...this.props} />
     );
   }
-
 }
