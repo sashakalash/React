03e0f9f4df97c1c0ef024baa29e647014b0ad3da
Саша Калаш <kalash@Sasha-Kalash.local>
2018-10-07 16:26:03 +0300

    HOC DateTimePretty added

--- ra-homeworks/hoc/relative-time/js/Video.js
+++ ra-homeworks/hoc/relative-time/js/Video.js
@@ -1,10 +1,30 @@
 'use strict';
 
+const MakingDatePretty = Component => {
+  return class extends React.Component {
+    getPrettyDate() {
+      const now = new Date();
+      const inputDate = new Date(this.props.date)
+      const diff = now - inputDate;
+      const hours = parseInt(diff / (1000 * 60 * 60));
+      const days = parseInt(hours / 24);
+      return hours <= 1 ? '12 минут назад' : hours <= 24 ? '5 часов назад' : `${days} дней назад`;
+    }
+
+    render() {
+      const prettyDate = this.getPrettyDate();
+      return <Component date={prettyDate}/>
+    }
+  }
+}
+
+const DateTimePretty = MakingDatePretty(DateTime);
+
 const Video = props => {
-    return (
-        <div className="video">
-            <iframe src={props.url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-            <DateTime date={props.date} />
-        </div>
-    )
+  return (
+    <div className="video">
+      <iframe src={props.url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+      <DateTimePretty date={props.date} />
+    </div>
+  )
 };
