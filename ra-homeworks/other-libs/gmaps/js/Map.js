2480095bac6366e488ab1ea52664513538ff455b
Саша Калаш <kalash@Sasha-Kalash.local>
2018-10-20 16:03:08 +0300

    created map showing & markers updating logic

--- ra-homeworks/other-libs/gmaps/js/Map.js
+++ ra-homeworks/other-libs/gmaps/js/Map.js
@@ -1,9 +1,48 @@
 class Map extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      points: []
+    }
+  }
+
+  componentDidMount() {
+    const myLatLng = { lat: 0, lng: 0 };
+    this.map = new google.maps.Map(this.node, {
+      center: myLatLng,
+      zoom: 0
+    });
+  }
+
+  componentWillReceiveProps(newProps) {
+    if (!this.state.points.lenght) {
+      this.state.points.map(office => office.setMap(null))
+    }
+    const points = [];
+    newProps.points.map(office => {
+      const marker = new google.maps.Marker(
+        {
+          map: this.map,
+          position: {
+            lat: office.lat,
+            lng: office.lon
+          }
+        }
+      )
+      points.push(marker);
+    })
+    this.setState({ points })
+  }
+
+  componentWillUnmount() {
+    this.map.clear();
+  }
+
   render() {
     return (
-      <div>
-        Карта
-        {this.props.points.map(office => <p>{JSON.stringify(office)}</p>)}
+      <div ref={node => this.node = node}>
+        map loading...
       </div>
     );
   }
