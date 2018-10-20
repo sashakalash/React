ff9194c22750d6edac1ad98faa37967d620a86dd
Саша Калаш <kalash@Sasha-Kalash.local>
2018-10-20 12:25:44 +0300

    corrected shouldComponentUpdate logic

--- ra-homeworks/lifecycle/shop/js/Cart.js
+++ ra-homeworks/lifecycle/shop/js/Cart.js
@@ -1,9 +1,9 @@
 class Cart extends React.Component {
   
   shouldComponentUpdate(newProps) {
-    const hasChanges = () => this.props.isOpen !== newProps.isOpen;
-    const itemsCountChanged = () => newProps.isOpen && this.props.items.length !== newProps.items.length;
-    return  hasChanges() || itemsCountChanged();
+    const hasChanges = this.props.isOpen !== newProps.isOpen;
+    const itemsCountChanged = newProps.isOpen && this.props.items.length !== newProps.items.length;
+    return  hasChanges || itemsCountChanged;
   }
 
   render() {
