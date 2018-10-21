126871da10f52ac37d86df0ebef2215dbd9c4e7a
Саша Калаш <kalash@Sasha-Kalash.local>
2018-10-21 19:30:54 +0300

    added Ant Design classes

--- ra-homeworks/instruments/mortgage/js/Inputs.js
+++ ra-homeworks/instruments/mortgage/js/Inputs.js
@@ -1,17 +1,29 @@
-const Inputs = () => (
-  <div>
-    <label>
-      Стоимость: <input type="number" name="price" value="2000000"/> руб.
-    </label>
-    <br/>
-    <label>
-      На руках:
-      <input type="number" name="money" value="200000"/> руб.
-    </label>
-    <br/>
-    <label>
-      Срок кредита:
-      <input type="number" name="duration" value="5"/> лет.
-    </label>
-  </div>
-);
+const { Input } = antd;
+
+class Inputs extends React.Component {
+  render() {
+    return <div style={{ marginBottom: 16 }}>
+      <Input 
+      type="number"
+      style={{ width: 500 }}
+      addonBefore="Стоимость:" 
+      addonAfter="руб." 
+      name='price'
+      />
+      <Input 
+      type="number"
+      style={{ width: 500 }} 
+      addonBefore="На руках:" 
+      addonAfter="руб." 
+      name="money"
+      />
+      <Input 
+      type="number"
+      style={{ width: 500 }} 
+      addonBefore="Срок кредита:" 
+      addonAfter="лет" 
+      name="duration"
+      />
+    </div>
+  }
+}
