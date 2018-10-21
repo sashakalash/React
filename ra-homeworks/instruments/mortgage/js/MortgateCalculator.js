4277c1d6198cc9e1c24e15ca7234c4626106e171
Саша Калаш <kalash@Sasha-Kalash.local>
2018-10-21 19:31:49 +0300

    added Ant Design Classes

--- ra-homeworks/instruments/mortgage/js/MortgateCalculator.js
+++ ra-homeworks/instruments/mortgage/js/MortgateCalculator.js
@@ -1,9 +1,41 @@
-const MortgateCalculator = () => (
-  <div>
-    <form action="">
-      <Autocomplete/>
-      <Inputs/>
-      <Button/>
-    </form>
-  </div>
-);
+const { Form, Button } = antd;
+const FormItem = Form.Item;
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 8 },
+  },
+  wrapperCol: {
+    xs: { span: 4 },
+    sm: { span: 6 },
+  },
+};
+
+class MortgateCalculator extends React.Component {
+  handleSubmit(e) {
+    e.preventDefault();
+    const data = new FormData(e.nativeEvent.target)
+    const dataToSend = {};
+    for (const el of data) {
+      dataToSend[el[0]] = el[1];
+    }
+    console.log(dataToSend)
+  }
+  render() {
+    return (
+      <div>
+        <Form layout="inline" onSubmit={this.handleSubmit}>
+          <FormItem {...formItemLayout}>
+            <Autocomplete />
+          </FormItem>
+          <FormItem {...formItemLayout}>
+            <Inputs />
+          </FormItem>
+          <FormItem>
+            <Button type="primary" htmlType="submit">Отправить</Button>
+          </FormItem>
+        </Form>
+      </div>
+    )
+  }
+}
--- ra-homeworks/instruments/mortgage/js/Select.js
+++ ra-homeworks/instruments/mortgage/js/Select.js
@@ -1,10 +1,27 @@
-const Autocomplete = () => (
-  <div>
-    Тип квартиры:<br/>
-    <select>
-      <option>Квартира в новостройке</option>
-      <option>Готовая квартира</option>
-      <option>Загородный дом</option>
-    </select>
-   </div>
-);
+const { Select } = antd;
+const Option = Select.Option;
+const types = [
+  {type: 'new_building', name: 'Квартира в новостройке'},
+  {type: 'old_building', name: 'Готовая квартира'},
+  {type: 'countryhouse', name: 'Загородный дом'}
+]
+
+class Autocomplete extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      types: types
+    }
+  }
+  render() {
+    return <Select
+    showSearch
+    style={{ width: 200 }}
+    placeholder="Выберите тип квартиры:"
+    optionFilterProp="children"
+    filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+  >
+    {this.state.types.map((item, index) => <Option value={item.type} key={index}>{item.name}</Option>)}
+  </Select>
+  }
+}
